f1f91b69b3494152cb443cd152f5dc53a9bfedbc
Yanina Lubenska <yaninaosadchaya@gmail.com>
2022-09-14 17:48:03 +0300

    fixed bugs, renamed directories

--- helper-bot-team-1/setup.py
+++ helper-bot-team-1/setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-
-setup(name="cli_assistant_bot",
+setup(name="helper-bot-team-1",
       version="1.0.0",
       description="Personal assistant bot that manages contacts, notes and can organize user's folders.",
       url="https://github.com/PavelDushinskiy/GoIT-Core-Project",
       author="Yanina Lubenska, Eugene Vyshnytsky, Pavel Dushinskiy",
-      packages=["assistant-bot"],
-      entry_points={"console_scripts": ["assistant-bot = helper-bot.main:run_app"]}
+      packages=find_namespace_packages(),
+      entry_points={"console_scripts": ["helper-bot-team-1=helper-bot.main:run_app"]}
       )
